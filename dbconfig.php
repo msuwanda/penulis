2be49f551c1b8ff3b2a17fc61ce680a5cdff1d7d
msuwanda <muhammad.suwanda@gmail.com>
2020-09-06 16:56:58 +0700

    databases

--- dbconfig.php
+++ dbconfig.php
@@ -1,11 +1,12 @@
 <?php 
     /** dev */
-    /*$_HOST   = "localhost" ; 
+    $_HOST   = "localhost" ; 
     $_DBNAME = "penerbit" ; 
     $_DBUSER = "root" ; 
-    $_DBPASS = "aldiani123" ; */
+    $_DBPASS = "aldiani123" ; 
+    /** pro 
     $_HOST   = "localhost" ; 
     $_DBNAME = "innovasi_penerbit" ; 
     $_DBUSER = "msuwanda" ; 
-    $_DBPASS = "Aldiani123" ;
+    $_DBPASS = "Aldiani123" ;*/
 ?>
