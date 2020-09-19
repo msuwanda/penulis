b80b4001e4d5ab00fdf72ed3094fbd63af9844b0
msuwanda <muhammad.suwanda@gmail.com>
2020-09-19 14:23:56 +0700

    Update dbconfig.php
    produksi

--- dbconfig.php
+++ dbconfig.php
@@ -1,12 +1,12 @@
 <?php 
-    /** dev */
+    /** dev 
     $_HOST   = "localhost" ; 
     $_DBNAME = "penerbit" ; 
     $_DBUSER = "root" ; 
-    $_DBPASS = "aldiani123" ; 
-    /** pro 
+    $_DBPASS = "aldiani123" ; */
+    /** pro */
     $_HOST   = "localhost" ; 
     $_DBNAME = "innovasi_penulis" ; 
     $_DBUSER = "msuwanda" ; 
-    $_DBPASS = "Aldiani123" ; */
+    $_DBPASS = "Aldiani123" ; 
 ?>
